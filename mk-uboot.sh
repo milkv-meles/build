d88194a5d0de8220bbf0b4a8ae9afc500019f5ba
Haaland Chen <haaland@milkv.io>
2024-06-22 16:46:36 +0800

    build meles of DDR 4/8/16GB together
    Signed-off-by: Haaland Chen <haaland@milkv.io>

--- mk-uboot.sh
+++ mk-uboot.sh
@@ -9,42 +9,21 @@ OUT=${LOCALPATH}/out
 
 [ ! -d ${OUT} ] && mkdir -p ${OUT}
 
-usage() {
-    echo "====USAGE: mk-uboot.sh -b <board_name> -d <ddr_rank>===="
-    echo "mk-uboot.sh -b milkv-meles -d dualrank"
-}
-
-while getopts "b:d:h" flag; do
-    case $flag in
-	b)
-	    export BOARD="$OPTARG"
-	    ;;
-	d)
-	    export DDRRANK="$OPTARG"
-	    ;;
-    esac
-done
-
-if [ ! $BOARD ] || [ ! $DDRRANK ]; then
-    usage
-    exit
-fi
-
-case ${BOARD} in
-	"milkv-meles")
-		UBOOT_DEFCONFIG=light_milkv_meles_${DDRRANK}_defconfig
-		;;
-	*)
-		echo "board '${BOARD}' not supported!"
-		exit -1
-		;;
-esac
-
 cd thead-u-boot
 
-make ${UBOOT_DEFCONFIG}
-make -j BUILD_TYPE=RELEASE
+make distclean
+make light_milkv_meles_16g_defconfig
+make -j$(nproc)
+find . -name "u-boot-with-spl.bin" | xargs -I{} cp -av {} ${OUT}/u-boot-with-spl-meles-16g.bin
 
-cp u-boot-with-spl.bin ${OUT}/u-boot-with-spl-${DDRRANK}-2020.01-g$(git rev-parse --short HEAD).bin
+make distclean
+make light_milkv_meles_dualrank_defconfig
+make -j$(nproc)
+find . -name "u-boot-with-spl.bin" | xargs -I{} cp -av {} ${OUT}/u-boot-with-spl-meles.bin
+
+make distclean
+make light_milkv_meles_singlerank_defconfig
+make -j$(nproc)
+find . -name "u-boot-with-spl.bin" | xargs -I{} cp -av {} ${OUT}/u-boot-with-spl-meles-4g.bin
 
 echo -e "\e[36m U-Boot build: success! \e[0m"
